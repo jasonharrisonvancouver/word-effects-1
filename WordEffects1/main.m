0a384a04347f3a81df79034d4b597b6dd66cd124
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-01-10 12:06:17 -0800

    Initial Commit

---
+++ WordEffects1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects1
+//
+//  Created by jason harrison on 2019-01-10.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
