57da29645e86143ae22ebd97f0a6e3afb61b0cac
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-01-10 12:12:31 -0800

    get user number 1-6 input and string input

--- WordEffects1/main.m
+++ WordEffects1/main.m
@@ -10,8 +10,43 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        while(YES){
+            
+            // 255 unit long array of characters
+            char inputChars[255];
+            
+            
+            
+            // 255 unit long array of characters
+            char inputNumber[255];
+            
+            printf("Input a number 1-6: ");
+            // limit input to max 255 characters
+            fgets(inputNumber, 255, stdin);
+            
+            // print as a c string
+            printf("Your string is %s\n", inputNumber);
+            
+            // convert char array to an NSString object
+            NSString *inputNumberString = [NSString stringWithUTF8String:inputNumber];
+            
+            // print NSString object
+            NSLog(@"Number was: %@", inputNumberString);
+            
+            
+            printf("Input a string: ");
+            // limit input to max 255 characters
+            fgets(inputChars, 255, stdin);
+            
+            // print as a c string
+            printf("Your string is %s\n", inputChars);
+            
+            // convert char array to an NSString object
+            NSString *inputString = [NSString stringWithUTF8String:inputChars];
+            
+            // print NSString object
+            NSLog(@"Input was: %@", inputString);
+        }
     }
     return 0;
 }
